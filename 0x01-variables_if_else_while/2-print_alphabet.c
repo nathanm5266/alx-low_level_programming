6b11cad484f08a9e3f985c17523fb2d1bdaa3ec5
nathanm5266 <nathanm5266@gmail.com>
2021-02-05 20:44:09 +0000

    update

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,12 +7,13 @@
 
 int main(void)
 {
-  char alphabet = 'a';
-  while (alphabet <= 'z')
-    {
-      putchar(alphabet);
-      alphabet++;
-    }
-  putchar('\n');
-  return (0);
+char alphabet = 'a';
+while (alphabet <= 'z')
+{
+putchar(alphabet);
+alphabet++;
+}
+putchar('\n');
+
+return (0);
 }
