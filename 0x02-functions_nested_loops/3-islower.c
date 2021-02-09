bbf690a9662deb8110dac74d8406ccd14107ff0d
nathanm5266 <nathanm5266@gmail.com>
2021-02-09 13:18:36 +0000

    push for fourth task

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
  * _islower - check the code for is lower case or not.
- *@c : the variable character to check
+ * @c : the variable character to check
  * Return: 1 if it is a lower case if not 0.
  */
 
@@ -16,5 +16,5 @@ else
 {
 return (0);
 }
-_putchar(('\n');
+_putchar('\n');
 }
