7741472f17eb29d5afc754d9fd892d00602ccd0b
nathanm5266 <nathanm5266@gmail.com>
2021-02-10 17:45:31 +0000

    update for this push

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,9 +12,9 @@ int i;
 printf("Infinite loop incoming :( \n");
 i = 0;
 /*while (i < 10) the code causing the output to go in to an infinite loop*/
-{
+/*{*/
 /*putchar(i);*/
-}
+/*}*/
 printf("Infinite loop avoided! \\o/ \n");
 return (0);
 }
