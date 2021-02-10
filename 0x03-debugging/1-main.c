a7c45453cadbcc639927b699b00bec0de8678c9c
nathanm5266 <nathanm5266@gmail.com>
2021-02-10 17:40:15 +0000

    push update

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -3,22 +3,18 @@
 /**
  * main - causes an infinite loop
  * Return: 0
+ * '\o/\n" - an emoji for avoiding
  */
 
 int main(void)
 {
 int i;
-
-printf("Infinite loop incoming :(\n");
-
+printf("Infinite loop incoming :( \n");
 i = 0;
-/**
-*while (i < 10)
-*{
-*putchar(i);
-*}
-*/
-printf("Infinite loop avoided! \\o/\n");
-
+/*while (i < 10) the code causing the output to go in to an infinite loop*/
+{
+/*putchar(i);*/
+}
+printf("Infinite loop avoided! \\o/ \n");
 return (0);
 }
