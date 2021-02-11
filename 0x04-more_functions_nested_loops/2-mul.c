e83bc61c66c11680ef9fd6c408bf341d3349b6eb
nathanm5266 <nathanm5266@gmail.com>
2021-02-11 19:59:57 +0300

    update for mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * mul - multiplies two intigers
+ * @a: one intiger
+ * @b: other intiger
+ * Return: product of a and b
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
