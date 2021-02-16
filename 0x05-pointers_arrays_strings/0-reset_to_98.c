03dbba84a66389001f62ee392dd26e83af5e1a39
nathanm5266 <nathanm5266@gmail.com>
2021-02-16 15:23:52 +0300

    initial commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - a fuction for out puting the value of n
+ * Return: always 0
+ * @n: the variable for accepting the number
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+A directory for the project pointers arrays and strings.
